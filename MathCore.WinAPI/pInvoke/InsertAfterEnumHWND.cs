9af00c72b1a8689a9aa474cfabc820081492f07a
Anton <it11@guardian.ru>
2021-06-18 11:43:34 +0300

    Функции управления окном и его поиск в системе

---
+++ MathCore.WinAPI/pInvoke/InsertAfterEnumHWND.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathCore.WinAPI.pInvoke
+{
+    public static class InsertAfterEnumHWND
+    {
+        public static readonly IntPtr
+            NoTopMost = new IntPtr(-2),
+            TopMost = new IntPtr(-1),
+            Top = new IntPtr(0),
+            Bottom = new IntPtr(1);
+    }
+}
--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
+using MathCore.WinAPI.Windows;
 
 namespace ConsoleTest
 {
@@ -7,11 +9,46 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            var notepad = Process.Start("notepad");
+            var notepad_process = Process.Start("notepad");
+
+            Console.WriteLine("Ждем...");
+            Console.ReadLine();
+
+            //var notepad = Window.Find(w => w.Text.EndsWith("Notepad++"));
+            //foreach (var w in notepad)
+            //    w.Text = "QWE";
+
+            var main_windows_hWnd = notepad_process.MainWindowHandle;
+
+            Window window = new Window(main_windows_hWnd);
+
+            Console.WriteLine("Текст окна = {0}", window.Text);
+            Console.WriteLine("Координаты окна = {0}", window.Rectangle);
+
+            // window.Text = "Hello world!";
+
+            //for(var x= window.X; x < 1692; x += 10)
+            //{
+            //    window.X = x;
+            //    Thread.Sleep(100);
+            //}
+
+            //Console.ReadLine();
+            //Console.WriteLine("Закрыть!");
+            //window.Close();
+
+            Console.WriteLine("Поверх всех окон.");
+            window.SetTopMost();
+            Console.ReadLine();
+
+            Console.WriteLine("НЕ Поверх всех окон.");
+            window.SetTopMost(false);
+            Console.ReadLine();
+
             Console.WriteLine("Завершено.");
             Console.ReadLine();
 
-            notepad.CloseMainWindow();
+            notepad_process.CloseMainWindow();
         }
     }
 }
